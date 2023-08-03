fd935b4380dac6c5ba2dd476eb57f861e416f9aa
Lucas Soler <lucas.p.soler@gmail.com>
2023-08-03 16:03:12 -0300

    scroll adjustments

--- src/app/components/Service.tsx
+++ src/app/components/Service.tsx
@@ -25,13 +25,26 @@ function Service({
   return (
     <section
       id={service.id}
-      className="flex-1 flex flex-col justify-center items-center p-2 gap-8"
+      className="flex-1 flex flex-col justify-center items-center p-2 gap-8 scroll-mt-56 2xl:scroll-mt-64"
     >
       <h2 className="flex-1 w-full xl:w-3/4 2xl:w-2/3 text-left">
         {capitalizeSentence(service.name)}
       </h2>
 
       <div className="flex-1 flex w-full xl:w-3/4 2xl:w-2/3 flex-col lg:flex-row gap-4 lg:gap-16">
+        <figure className="lg:hidden flex-1 flex flex-col gap-4">
+          <Image
+            src={`/${service.id}.jpg`}
+            alt={service.name}
+            title={service.name}
+            width={626}
+            height={425}
+            className="m-auto"
+          />
+          <figcaption className="text-center font-bold">
+            {capitalizePersonName(service.name)}
+          </figcaption>
+        </figure>
         {orientation === "right" ? (
           <ServiceImage serviceID={service.id} serviceName={service.name} />
         ) : (
@@ -66,7 +79,7 @@ interface ServiceImageProps {
 
 function ServiceImage({ serviceID, serviceName }: ServiceImageProps) {
   return (
-    <figure className="flex-1 flex flex-col gap-4">
+    <figure className="hidden lg:flex flex-1 flex-col gap-4">
       <Image
         src={`/${serviceID}.jpg`}
         alt={serviceName}
