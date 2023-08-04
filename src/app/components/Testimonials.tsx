ca59a81bd6ac40db00e74c8617bc2ec771b7f41f
Lucas Soler <lucas.p.soler@gmail.com>
2023-08-04 18:50:21 -0300

    general fixes

--- src/app/components/Testimonials.tsx
+++ src/app/components/Testimonials.tsx
@@ -1,10 +1,21 @@
 "use client";
 
 import Image from "next/image";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-function Testimonials() {
+interface Testimonial {
+  id: number;
+  photo: string;
+  alternative_image_text: string;
+  name: string;
+  description: string;
+}
+interface TestimonialsProps {
+  testimonials: Testimonial[];
+}
+
+function Testimonials({ testimonials }: TestimonialsProps) {
   return (
     <Carousel
       showArrows={true}
@@ -14,41 +25,22 @@ function Testimonials() {
       autoPlay={true}
       interval={6100}
     >
-      <div>
-        <Image src="/shirley.png" alt={""} width={400} height={400} />
-        <div className="my-carousel">
-          <h3>Shirley Fultz</h3>
-          <h4>Designer</h4>
-          <p>
-            Its freeing to be able to catch up on customized news and not be
-            distracted by a social media element on the same site
-          </p>
-        </div>
-      </div>
-
-      <div>
-        <Image src="/daniel.png" alt={""} width={400} height={400} />
-        <div className="my-carousel">
-          <h3>Daniel Keystone</h3>
-          <h4>Designer</h4>
-          <p>
-            The simple and intuitive design makes it easy for me use. I highly
-            recommend Fetch to my peers.
-          </p>
-        </div>
-      </div>
-
-      <div>
-        <Image src="/theo.png" alt={""} width={400} height={400} />
-        <div className="my-carousel">
-          <h3>Theo Sorel</h3>
-          <h4>Designer</h4>
-          <p>
-            I enjoy catching up with Fetch on my laptop, or on my phone when Im
-            on the go!
-          </p>
-        </div>
-      </div>
+      {testimonials.map((testimonial) => {
+        return (
+          <div key={`testimonial-${testimonial.id}`}>
+            <Image
+              src={`/${testimonial.photo}`}
+              alt={`${testimonial.alternative_image_text}`}
+              width={400}
+              height={400}
+            />
+            <div className="my-carousel">
+              <h3>{testimonial.name}</h3>
+              <p>{testimonial.description}</p>
+            </div>
+          </div>
+        );
+      })}
     </Carousel>
   );
 }
