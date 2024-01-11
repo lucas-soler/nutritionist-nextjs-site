1f388017d2ed2ad57a806d39e74d9f5861c1b94c
Lucas Soler <lucas.p.soler@gmail.com>
2024-01-11 16:02:06 -0300

    Test changes

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -104,7 +104,7 @@ export default async function Home({ params: { lang } }: PageProps) {
           className="flex-1 flex flex-col justify-center items-center px-2 gap-4 scroll-mt-56 2xl:scroll-mt-64"
         >
           <h2 className="flex-1 w-full xl:w-3/4 2xl:w-2/3">
-            {capitalizeSentence(dictionaire.mainMenu.services)}
+            {capitalizeSentence(dictionaire.mainMenu.services)} teste
           </h2>
           <div className="flex-1 flex w-full xl:w-3/4 2xl:w-2/3 flex-col lg:flex-row gap-14">
             <ServicesList services={dictionaire.services} />
